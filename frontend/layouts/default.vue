df0ec8ae40c78565a1447930837ddc24f760d364
Alfuveam <felipe.b.t@hotmail.com>
2024-09-08 22:16:04 -0300

    Defined base layout, add base pages and logo

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="p-4 bg-gray-700 min-h-screen text-gray-100">
-    <slot />
+  <div class="p-3 bg-gray-700 min-h-screen text-gray-100">
+    <header>
+      <div class="w-full h-14 bg-gray-800 rounded">
+        <div class="p-1 flex justify-between">
+            <NuxtLink to="/" class="flex flex-center">
+                <img src="@/assets/images/logo_tcc.webp" class="h-12 w-12" alt="">
+                <span class="justify-center text-3xl font-bold text-gray-300 p-1">TCC</span>
+            </NuxtLink>
+            <div></div>
+            <div class="flex flex-center justify-between p-3">
+                <NuxtLink to="/login" class="mr-4">Entrar</NuxtLink>
+                <NuxtLink to="/createaccount">Criar Conta</NuxtLink>
+            </div>
+        </div>
+      </div>
+    </header>
+
+    <div class="w-full min-h-screen">
+      <slot />
+    </div>
+
+    <footer>
+        <div class="w-full h-24 bg-gray-800 rounded">
+            <div class="flex justify-between">
+                <div></div>
+                <div class="flex justify-between p-3">
+                    <NuxtLink to="/legal" class="mr-4">legal</NuxtLink>
+                    <NuxtLink to="/contact">contact</NuxtLink>
+                </div>
+            </div>
+            <hr class="ml-4 mr-4 border-gray-600"/>
+            <div class="flex justify-center mt-2">TCC - Todos os direitos reservados</div>
+        </div>
+    </footer>
   </div>
 </template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="self-center">
-    <div class="bg-slate-800 grid justify-items-center w-86 h-96 sm:w-96 sm:h-96 rounded space-y-0.2 p-28" >
-      <NuxtLink class="bg-gray-600 hover:bg-gray-500 w-32 h-12 rounded text-center p-3" to="/login">Entrar</NuxtLink>
-      <NuxtLink class="bg-gray-600 hover:bg-gray-500 w-32 h-12 rounded text-center p-3" to="/createaccount">Criar Conta</NuxtLink>
-      <NuxtLink class="bg-gray-600 hover:bg-gray-500 w-32 h-12 rounded text-center p-3" to="/legal">Legal</NuxtLink>
+  <div class="flex items-top md:items-center pt-10 md:pt-0 justify-center min-h-screen w-full">
+    <div class="bg-slate-800 grid justify-items-center w-5/5 h-96 md:w-5/5 md:h-96 lg:mb-96 2xl:mb-0 rounded space-y-0.2 p-28 " >
+      <NuxtLink class="bg-gray-600 hover:bg-gray-500 w-32 md:w-40 h-12 rounded text-center p-3" to="/login">Entrar</NuxtLink>
+      <NuxtLink class="bg-gray-600 hover:bg-gray-500 w-32 md:w-40 h-12 rounded text-center p-3" to="/createaccount">Criar Conta</NuxtLink>
+      <NuxtLink class="bg-gray-600 hover:bg-gray-500 w-32 md:w-40 h-12 rounded text-center p-3" to="/legal">Legal</NuxtLink>
     </div>
   </div>
 </template>
